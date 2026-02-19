4bca1ae276393fb61c316c91af82116d5e4cf59e
acfurlaneto <ana.furlaneto@aluno.senai.br>
2026-02-19 16:14:19 -0300

    Atualizando

--- api_telemetria/api/serializers.py
+++ api_telemetria/api/serializers.py
@@ -3,30 +3,30 @@ from api_telemetria import models
 
 class MarcaSerializer(serializers.ModelSerializer):
     class Meta:
-        model = models.marca
+        model = models.Marca
         fields = "__all__"
 
 class ModeloSerializer(serializers.ModelSerializer):
     class Meta:
-        model = models.modelo
+        model = models.Modelo
         fields = "__all__"
 
 class VeiculoSerializer(serializers.ModelSerializer):
     class Meta:
-        model = models.veiculo
+        model = models.Veiculo
         fields = "__all__"
 
 class UnidadeMedidaSerializer(serializers.ModelSerializer):
     class Meta:
-        model = models.unidade_medida
+        model = models.UnidadeMedida
         fields = "__all__"
 
 class MedicacaoSerializer(serializers.ModelSerializer):
     class Meta:
-        model = models.medicacao
+        model = models.Medicacao
         fields = "__all__"
 
 class MedicacaoVeiculoSerializer(serializers.ModelSerializer):
     class Meta:
-        model = models.medicacao_veiculo
+        model = models.MedicacaoVeiculo
         fields = "__all__"
--- api_telemetria/api/viewsets.py
+++ api_telemetria/api/viewsets.py
@@ -4,24 +4,24 @@ from api_telemetria.api import serializers
 
 class MarcaViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.MarcaSerializer
-    queryset = models.marca.objects.all()
+    queryset = models.Marca.objects.all()
 
 class ModeloViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.ModeloSerializer
-    queryset = models.modelo.objects.all()
+    queryset = models.Modelo.objects.all()
 
 class VeiculoViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.VeiculoSerializer
-    queryset = models.veiculo.objects.all()
+    queryset = models.Veiculo.objects.all()
 
 class UnidadeMedidaViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.UnidadeMedidaSerializer
-    queryset = models.unidade_medida.objects.all()
+    queryset = models.UnidadeMedida.objects.all()
 
 class MedicacaoViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.MedicacaoSerializer
-    queryset = models.medicacao.objects.all()
+    queryset = models.Medicacao.objects.all()
 
 class MedicacaoVeiculoViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.MedicacaoVeiculoSerializer
-    queryset = models.medicacao_veiculo.objects.all()
+    queryset = models.MedicacaoVeiculo.objects.all()
